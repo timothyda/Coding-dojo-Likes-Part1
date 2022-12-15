09cc716a9a10fde821c92803c180227c374a6562
timothyda <116868408+timothyda@users.noreply.github.com>
2022-12-14 21:22:27 -0500

    Add files via upload

---
+++ Likes/part1/JS.js
@@ -0,0 +1,4 @@
+function count() {
+    like.innerText++
+}
+
---
+++ Likes/part2/JS.js
@@ -0,0 +1,15 @@
+function count1() {
+    likes1.innerText++
+}
+
+function count2() {
+    likes2.innerText++
+}
+
+function count3() {
+    likes3.innerText++
+}
+
+
+
+
